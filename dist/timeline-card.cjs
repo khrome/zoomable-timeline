195fd4629f404561089a733083dd2fc19e7d7add
Abbey Sparrow <abbey@khrome.net>
2023-08-07 18:34:58 -0600

    transitions

--- dist/timeline-card.cjs
+++ dist/timeline-card.cjs
@@ -20,10 +20,14 @@ class TimelineCard extends _elements.HTMLElement {
     this._container = document.createElement('div');
     this._container.innerHTML = '<slot></slot>';
     this.shadowRoot.appendChild(this._container);
+    this.transitionPos = 0;
   }
   async setContent(html) {
+    let transitions = this.getAttribute('transitions');
+    transitions = transitions ? transitions.split(',').map(s => s.trim()) : ['flipInY'];
     return await new Promise((resolve, reject) => {
       try {
+        const transition = transitions[this.transitionPos % transitions.length];
         if (html !== null) {
           this._container.innerHTML = html;
         } else {
@@ -33,14 +37,15 @@ class TimelineCard extends _elements.HTMLElement {
         const endFn = () => {
           animationTarget.removeEventListener('animationend', endFn);
           animationTarget.classList.remove(`animate__${'animated'}`);
-          animationTarget.classList.remove(`animate__${'flipInY'}`);
+          animationTarget.classList.remove(`animate__${transition}`);
           animationTarget.classList.remove(`animate__${'faster'}`);
           resolve();
         };
         animationTarget.addEventListener('animationend', endFn, false);
         animationTarget.classList.add(`animate__${'animated'}`);
-        animationTarget.classList.add(`animate__${'flipInY'}`);
+        animationTarget.classList.add(`animate__${transition}`);
         animationTarget.classList.add(`animate__${'faster'}`);
+        this.transitionPos++;
       } catch (ex) {
         reject(ex);
       }
